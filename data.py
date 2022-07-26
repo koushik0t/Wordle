fbb4049708af743d7634e72bd1ecf2f6bd55c593
Orange232 <koushik0t@gmail.com>
2022-07-25 23:41:53 -0400

    more transfer of files

--- data.py
+++ data.py
@@ -1,4 +1,6 @@
 import re
+import pandas as pd
+import wordfreq
 
 def create_wordfile(filename):
     d = open("words_alpha.txt", "r")
@@ -9,5 +11,20 @@ def create_wordfile(filename):
     d.close()
     five.close()
 
-def 
+def calc_bits(word, dat, response):
+    if len(response) < 5:
+        response = response + "g"
+        calc_bits()
+
+def possibilities(word, dat, response):
+    for 
+
+create_wordfile("5words.txt")
+words = open("5words.txt", "r")
+guess_stats = pd.DataFrame(columns = ["word", "bits", "freq"])
+guess_stats["word"] = words.readlines()
+guess_stats["word"] = guess_stats["word"].str.strip()
+print(guess_stats.head())
+for i in guess_stats.iterrows():
+    
 
