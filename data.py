8bc084bd37390464c9ed4b306b9955c443dd20a9
Orange232 <kkt.dude74@gmail.com>
2022-08-02 14:17:02 -0400

    it works!

--- data.py
+++ data.py
@@ -9,12 +9,12 @@ counter = 0
 
 def create_files(filename):
     d = open("words_alpha.txt", "r")
-    five = open(filename + ".txt", "w")
-    for line in d.readlines():
-        if re.search(r"^.{5}\n$", line):
-            five.write(line)
+    #five = open(filename + ".txt", "w")
+    #for line in d.readlines():
+    #    if re.search(r"^.{5}\n$", line):
+    #        five.write(line)
     d.close()
-    five.close()
+    #five.close()
     words = open(filename + ".txt", "r")
     guess_stats = pd.DataFrame(columns = ["word", "bits"])
     guess_stats["word"] = words.readlines()
@@ -41,17 +41,18 @@ def calc_bits(word, dat, d):
     sum = 0.0
     for possible in dat:
         key = construct_word_code(word, possible)
+        #print(key, word, possible)
         d[key] += 1
     for k in d.keys():
         if d[k] > 0:
             p = d[k]/len(dat)
             sum += -1*p*math.log(p, 2)
             d[k] = 0
-    global counter
+    '''global counter
     counter += 1
     if counter % 50 == 0:
         print(word)
-        print(sum)
+        print(sum)'''
     return sum
 
 def check_word(to_check, regex, yellow):
@@ -66,19 +67,27 @@ def check_word(to_check, regex, yellow):
 
 def possibilities(word, dat, pat):
     outcome = construct_regex(word, pat)
-    dat["drop"] = dat["word"].map(lambda x: check_word(x, outcome[0], outcome[1]))
+    #print(outcome)
+    dat["drop"] = dat["word"].map(lambda x: not check_word(x, outcome[0], outcome[1]))
     return dat[dat["drop"] == False]
 
 def construct_word_code(word, target):
-    r = ""
+    wlist = list(word)
+    tlist = list(target)
+    r = []
     for i in range(0, len(word)):
         if word[i] == target[i]:
-            r = r + "!"
-        elif word[i] in target and r.count("?") < target.count(word[i]):
-            r = r + "?"
+            r.append("!")
+            wlist[i] = "."
+            tlist[i] = "."
         else:
-            r = r + "."
-    return r
+            r.append(".")
+    for i in range(0, len(wlist)):
+        if wlist[i] != "." and wlist[i] in tlist:
+            tlist[tlist.index(wlist[i])] = "."
+            wlist[i] = "."
+            r[i] = "?"
+    return "".join(r)
 
 def construct_regex(word, response):
     d = dict()
@@ -86,33 +95,43 @@ def construct_regex(word, response):
     if response == "":
         return r, d
     gray = ""
+    yellow = ""
     for i in range(0, len(response)):
-        if response[i] == ".":
+        if response[i] == "." and word[i] not in yellow:
             gray = gray + word[i]
+        elif response[i] == "?":
+            yellow = yellow + word[i]
+            if word[i] in gray:
+                gray = gray.replace(word[i], "")
+    for c in list(yellow):
+        d[c] = word.count(c)
     for i in range(0, len(response)):
         if response[i] == "!":
             r = r + word[i]
-        elif response[i] == "?":
-            r = r + "[^" + gray + word[i] + "]"
-            if word[i] not in d.keys():
-                d[word[i]] = 0
-            if word[i] in gray:
-                d[word[i]] -= 1
-            else:
-                d[word[i]] += 1
         else:
-            r = r + "[^" + gray + "]"
+            if response[i] == "." and word[i] in yellow:
+                d[word[i]] = -1*(abs(d[word[i]]) - 1)
+            r = r + "[^" + gray + word[i] + "]"
+
     return r, d
 
 def simwordle(answer, dat):
+    print(answer)
     guesses = []
     d = create_dictionary(dict(), "")
-    guesses.append(dat.loc[dat["bits"]==dat["bits"].max(), "word"])
-    guesses["drop"] = False
-    while len(guesses) < 7 and guesses[len(guesses)-1]["word"] != answer:
-        dat = possibilities(guesses[len(guesses)-1]), dat, construct_word_code(guesses[len(guesses)-1]["word"], answer)
-        dat["bits"] = dat["word"].map(lambda x: calc_bits(x, dat["word"], d))
-        guesses.append(dat.loc[dat["bits"]==dat["bits"].max()])
+    next = dat.loc[dat["bits"]==dat["bits"].max()]
+    guesses.append((dat.iloc[next.index[0]]["word"], dat.iloc[next.index[0]]["bits"]))
+    while len(guesses) < 7 and guesses[len(guesses)-1][0] != answer:
+        #print(guesses)
+        cod = construct_word_code(guesses[len(guesses)-1][0], answer)
+        dat = possibilities(guesses[len(guesses)-1][0], dat, cod)
+        #print(cod, dat["word"])
+        dat = dat.reset_index(drop=True)
+        for row in dat.iterrows():
+            dat.at[row[0], "bits"] = calc_bits(row[1]["word"], dat["word"], d)
+        next = dat.loc[dat["bits"]==dat["bits"].max()]
+        guesses.append((dat.iat[next.index[0], 0], dat.iloc[next.index[0]]["bits"]))
+    print(guesses)
     return len(guesses)
     """d = dict()
     r = ""
--- wordlesim.py
+++ wordlesim.py
@@ -1,12 +1,22 @@
 import wordfreq
 import data
 import pandas as pd
+import matplotlib.pyplot as plt
 import pickle
 
-#create_files("5words")
+def simulate(wordlist, dat):
+    stats = []
+    for word in wordlist:
+        stats.append(data.simwordle(word.strip(), dat))
+    return stats
+
+#data.create_files("5words")
+
 words = open("5words.txt", "r")
 pic = open("5words.pickle", "rb")
 df = pd.DataFrame(pickle.load(pic))
-print(df.head())
-
-val = data.simwordle("chalk", df)
+print(df.head(), len(df))
+stuff = simulate(words.readlines(), df)
+stats = open("max5wordle.pickle", "wb")
+pickle.dump(stuff, stats)
+print(stuff)
