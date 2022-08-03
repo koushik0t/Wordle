193c29cd28785e905055c2e516c3db66e39169c0
Orange232 <kkt.dude74@gmail.com>
2022-08-03 09:34:23 -0400

    initial completed wordle

--- wordlesim.py
+++ wordlesim.py
@@ -2,6 +2,7 @@ import wordfreq
 import data
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy
 import pickle
 
 def simulate(wordlist, dat):
@@ -12,11 +13,20 @@ def simulate(wordlist, dat):
 
 #data.create_files("5words")
 
-words = open("5words.txt", "r")
-pic = open("5words.pickle", "rb")
-df = pd.DataFrame(pickle.load(pic))
-print(df.head(), len(df))
-stuff = simulate(words.readlines(), df)
-stats = open("max5wordle.pickle", "wb")
-pickle.dump(stuff, stats)
-print(stuff)
+#words = open("5words.txt", "r")
+#pic = open("5words.pickle", "rb")
+#df = pd.DataFrame(pickle.load(pic))
+#print(df.head(), len(df))
+#stuff = simulate(words.readlines(), df)
+stuff = open("max5wordle.pickle", "rb")
+stats = pickle.load(stuff)
+xs = [1, 2, 3, 4, 5, 6, 7]
+ys = []
+for i in range(1, 8):
+    ys.append(stats.count(i))
+print(ys)
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.bar(xs, ys)
+plt.plot(xs, ys)
+plt.show()
